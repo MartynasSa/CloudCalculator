3db4ea749b7de9a79937dc6086d3eddc18d1d1e9
Martynas Samuilovas <samuilovas@gmail.com>
2025-12-05 00:08:48 +0200

    starting VM logic

---
+++ src/Application/Models/Enums/CloudResource.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Models.Enums;
+
+public enum CloudResource
+{
+    None = 0,
+    VirtualMachines = 1,
+}
---
+++ src/Application/Models/Enums/VmOperatingSystem.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Models.Enums;
+
+public enum VmOperatingSystem
+{
+    None = 0,
+    Linux = 1,
+    Windows = 2
+}
