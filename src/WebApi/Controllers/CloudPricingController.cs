25b816defcad50a4da9369f9f004337cc2abcb59
Martynas Samuilovas <samuilovas@gmail.com>
2025-12-23 23:32:58 +0200

    update

--- src/WebApi/Controllers/CloudPricingController.cs
+++ src/WebApi/Controllers/CloudPricingController.cs
@@ -1,28 +1,12 @@
-﻿using Application.Models.Dtos;
+﻿using Application.Facade;
 using Application.Models.Enums;
 using Microsoft.AspNetCore.Mvc;
-using Application.Facade;
 
 namespace WebApi.Controllers;
 
 [Route("api")]
 public class CloudPricingController(ICloudPricingFileFacade cloudPricingFileFacade) : Controller
 {
-    [HttpGet("cloud-pricing")]
-    public async Task<IActionResult> GetPricing([FromQuery] PricingRequest? pagination, CancellationToken ct)
-    {
-        pagination ??= new PricingRequest();
-        var result = await cloudPricingFileFacade.GetOrCreatePagedAsync(pagination, ct);
-        return Ok(result);
-    }
-
-    [HttpGet("cloud-pricing:options")]
-    public async Task<IActionResult> Options(CancellationToken ct)
-    {
-        var result = await cloudPricingFileFacade.GetDistinctFiltersAsync(ct);
-        return Ok(result);
-    }
-
     [HttpGet("cloud-pricing:product-family-mappings")]
     public async Task<IActionResult> GetProductFamilyMappings([FromQuery] UsageSize? usage, CancellationToken ct)
     {
